f3585b691b3c1e084310bf82545486f0bb551811
sabitameem <sabitameem578@gmail.com>
2023-06-12 10:40:50 +0530

    delted function from my selected classes added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgocvky.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -72,6 +72,13 @@ async function run() {
       const classCard = req.body;
       console.log(classCard);
       const result = await selectedClassCollection.insertOne(classCard);
+      res.send(result);
+    })
+
+    app.delete('/classes/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result=await selectedClassCollection.deleteOne(query);
       res.send(result);
     })
 
