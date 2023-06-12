67a4e5595d7340150e9caf8337fe9dbed3b5d521
sabitameem <sabitameem578@gmail.com>
2023-06-13 02:34:34 +0530

    installed stripe and create payment intent api

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
-    return res.status(401).send({ error: true, message: 'unauthorized access' });
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
   }
-  const token = authorization.split(' ')[1];
+  const token = authorization.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ error: true, message: 'unauthorized access' })
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
-  })
-}
-
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgocvky.mongodb.net/?retryWrites=true&w=majority`;
@@ -52,124 +55,110 @@ async function run() {
     // this route is for selected classes that user selected
     const selectedClassCollection = client.db("linguaDb").collection("classes");
 
-
-
     //jwt
-    app.post('/jwt',(req,res)=>{
+    app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-      res.send({ token })
-    })
-
-
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     //verify admin
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = { email: email }
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      if (user?.role !== 'admin') {
-        return res.status(403).send({ error: true, message: 'forbidden message' });
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
       }
       next();
-    }
+    };
 
-
-    const verifyInstructor =async(req, res, next) => {
+    const verifyInstructor = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = { email: email }
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      if (user?.role !== 'instructor') {
-        return res.status(403).send({ error: true, message: 'forbidden message' });
+      if (user?.role !== "instructor") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
       }
       next();
-
-    }
-
-
-
-
-   //user related routes
-
-   app.get('/users',verifyJWT,verifyAdmin, async(req,res)=>{
-    const result =await usersCollection.find().toArray();
-    res.send(result);
-   })
-
-
-
-
-   app.post('/users', async(req,res)=>{
-    const user = req.body;
-    const query = { email: user.email }
-    const existingUser = await usersCollection.findOne(query);
-    if (existingUser) {
-      return res.send({ message: 'user already exists' })
-    }
-
-    const result = await usersCollection.insertOne(user);
-    res.send(result);
-   })
-
-
-   //admin
-   app.get('/users/admin/:email',verifyJWT, async(req,res)=>{
-    const email =req.params.email;
-    if (req.decoded.email !== email) {
-      res.send({ admin: false })
-      return
-    }
-    const query = { email: email }
-      const user = await usersCollection.findOne(query);
-      const result = { admin: user?.role === 'admin' }
-      res.send(result);
-   })
-   
-   
-
-
-
-   app.patch('/users/admin/:id', async(req,res)=>{
-    const id = req.params.id;
-    console.log(id);
-    const filter = { _id: new ObjectId(id) };
-    const updateDoc = {
-      $set: {
-        role: 'admin'
-      },
     };
-    const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
-   })
 
-   //instructor
-   app.get('/users/instructor/:email',verifyJWT, async(req,res)=>{
-    const email =req.params.email;
-    if (req.decoded.email !== email) {
-      res.send({ instructor: false })
-      return
-    }
-    const query = { email: email }
+    //user related routes
+
+    app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+        return;
+      }
+      const query = { email: email };
       const user = await usersCollection.findOne(query);
-      const result = { instructor: user?.role === 'instructor' }
+      const result = { admin: user?.role === "admin" };
       res.send(result);
-   })
+    });
 
-
-
-   app.patch('/users/instructor/:id', async(req,res)=>{
-    const id = req.params.id;
-    console.log(id);
-    const filter = { _id: new ObjectId(id) };
-    const updateDoc = {
-      $set: {
-        role: 'instructor'
-      },
-    };
-    const result = await usersCollection.updateOne(filter, updateDoc);
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
-   })
+    });
 
+    //instructor
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false });
+        return;
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     // reviews
     app.get("/reviews", async (req, res) => {
@@ -200,7 +189,9 @@ async function run() {
       }
       const decodedEmail = req.decoded.email;
       if (email !== decodedEmail) {
-        return res.status(403).send({ error: true, message: 'forbidden access' })
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
       const query = { email: email };
       const result = await selectedClassCollection.find(query).toArray();
@@ -219,6 +210,20 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await selectedClassCollection.deleteOne(query);
       res.send(result);
+    });
+
+    // create payment intent
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     });
 
     // having problem with https://lingua-viva-server.vercel.app/reviews
