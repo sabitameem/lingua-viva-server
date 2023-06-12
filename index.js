d55f62eaf4a35441962024aeb58e62a9284834f5
sabitameem <sabitameem578@gmail.com>
2023-06-12 13:25:59 +0530

    added get users api

--- index.js
+++ index.js
@@ -36,6 +36,15 @@ async function run() {
 
 
    //user related routes
+
+   app.get('/users', async(req,res)=>{
+    const result =await usersCollection.find().toArray();
+    res.send(result);
+   })
+
+
+
+
    app.post('/users', async(req,res)=>{
     const user = req.body;
     const query = { email: user.email }
